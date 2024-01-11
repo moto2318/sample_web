3200f14fa21a75f3a8e3c51a45f5038eef87067c
モト2318 <suigekka0101@gmail.com>
2024-01-11 23:30:42 +0900

    viewの調整

--- www/sample_web/routes/web.php
+++ www/sample_web/routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\IndexController::class, 'index']);
-Route::get('/top', [App\Http\Controllers\TopController::class, 'index']); 
+Route::get('/top', [App\Http\Controllers\TopController::class, 'index']); // ←追加
